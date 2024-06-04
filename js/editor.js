3a8f5c8927e8005a530da7a9c4a94704b14d6306
David Morrow <dperrymorrow@gmail.com>
2024-06-04 09:32:29 -0700

    working

--- js/editor.js
+++ js/editor.js
@@ -1,62 +1,45 @@
-// import mirror from "./mirror.js";
-
 const $editor = document.querySelector("#editor");
 const $preview = $editor.querySelector(".preview");
 const $code = $editor.querySelector(".code");
 const lineHeight = 24;
-
-$code.style.height =
-  Math.ceil($code.getBoundingClientRect().height / lineHeight) * lineHeight;
+const characterWidth = 16;
+const lineWrap = 80;
 
 let syntax;
 let changeDetected = true;
 
-function snap() {
-  const top = $code.scrollTop;
-
-  const bottomedOut = $code.scrollHeight - top === $code.clientHeight;
-  console.log(bottomedOut);
-
-  // if (bottomedOut) return;
-
-  const snap = Math.floor(top / lineHeight) * lineHeight;
-  $code.scrollTo({ top: snap, behavior: "smooth" });
-}
-
-// here is where we mirror with syntax only the viewable area of the code
 function mirror() {
-  const scrollPos = $code.scrollTop;
-  const snap = Math.round($code.scrollTop / lineHeight) * lineHeight;
+  const scrollPos = $editor.scrollTop;
+  const snap = Math.round(scrollPos / lineHeight) * lineHeight;
 
-  const height = $code.getBoundingClientRect().height;
+  const height = $editor.getBoundingClientRect().height;
   const visibleLines = Math.ceil(height / lineHeight);
 
   const startLine = Math.floor(scrollPos / lineHeight);
   const endLine = Math.ceil(startLine + visibleLines);
 
   syntax = changeDetected
-    ? Prism.highlight($code.value, Prism.languages.javascript, "javascript")
+    ? Prism.highlight($code.innerText, Prism.languages.javascript, "javascript")
     : syntax;
 
   changeDetected = false;
-  $preview.innerHTML = syntax.split("\n").splice(startLine, endLine).join("\n");
+
+  const codeLines = $code.innerHTML.split("\n");
+  const syntaxLines = syntax.split("\n");
+
+  const lines = codeLines.map((line, index) =>
+    index >= startLine && index <= endLine ? syntaxLines[index] : line,
+  );
+
+  $preview.innerHTML = lines.flat().join("\n");
 }
 
 $code.addEventListener("input", () => {
   changeDetected = true;
   mirror();
-  snap();
 });
 
-$code.addEventListener("scroll", (ev) => {
-  mirror();
-});
-
-$code.addEventListener("scrollend", snap);
-
-window.addEventListener("resize", () => {
-  snap();
-  mirror();
-});
+$editor.addEventListener("scroll", mirror);
+window.addEventListener("resize", mirror);
 
 mirror();
--- js/syntax.js
+++
@@ -1,7 +0,0 @@
-export default function ($textarea) {
-  return Prism.highlight(
-    $textarea.value,
-    Prism.languages.javascript,
-    "javascript",
-  );
-}
