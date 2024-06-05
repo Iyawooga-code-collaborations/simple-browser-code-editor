4ca3a067addcfb3e708efaaf789b4b6dbb87d2b3
David Morrow <dperrymorrow@gmail.com>
2024-06-05 14:02:22 -0700

    update for simple

--- js/editor.js
+++ js/editor.js
@@ -1,49 +1,12 @@
-const $editor = document.querySelector("#editor");
-const $preview = $editor.querySelector(".preview");
-const $code = $editor.querySelector(".code");
-const lineHeight = parseInt(window.getComputedStyle($code).lineHeight);
-const characterWidth = 16;
-const lineWrap = 80;
-
-let syntax;
-let previousRaw = "";
+const $preview = document.querySelector("#editor .preview");
+const $code = document.querySelector("#editor .code");
 
 function mirror() {
-  const scrollPos = $editor.scrollTop;
-  // const snap = Math.round(scrollPos / lineHeight) * lineHeight;
-  const height = $editor.getBoundingClientRect().height;
-  const visibleLines = Math.ceil(height / lineHeight);
-
-  const startLine = Math.floor(scrollPos / lineHeight);
-  const endLine = Math.ceil(startLine + visibleLines);
-
-  const raw = $code.value;
-
-  // $editor.scroll({ top: snap });
-  $code.style.height = `${$code.scrollHeight}px`;
-
-  syntax =
-    raw !== previousRaw
-      ? Prism.highlight(raw, Prism.languages.javascript, "javascript")
-      : syntax;
-
-  previousRaw = raw;
-
-  const codeLines = raw.split("\n");
-  const syntaxLines = syntax.split("\n");
-  // .map((line) => `<span class="line">${line}</span>`);
-
-  const previewLines = codeLines.map((line, index) =>
-    index >= startLine && index <= endLine ? syntaxLines[index] : line,
+  $preview.innerHTML = Prism.highlight(
+    $code.value,
+    Prism.languages.javascript,
+    "javascript",
   );
-
-  $preview.innerHTML = previewLines.join("\n");
 }
 
-$code.addEventListener("input", () => {
-  changeDetected = true;
-  mirror();
-});
-
-$editor.addEventListener("scrollend", mirror);
-window.addEventListener("resize", mirror);
+$code.addEventListener("input", mirror);
