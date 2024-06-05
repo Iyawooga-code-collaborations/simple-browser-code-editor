820fe2ad3524621f2bc8fe359c9d23371e419989
David Morrow <dperrymorrow@gmail.com>
2024-06-05 14:20:54 -0700

    simple working version

--- js/editor.js
+++ js/editor.js
@@ -2,11 +2,22 @@ const $preview = document.querySelector("#editor .preview");
 const $code = document.querySelector("#editor .code");
 
 function mirror() {
+  // make textarea grow to height of content so we can scroll together
+  $code.style.height = $code.scrollHeight;
+  // update the preview underlay with the syntax highlight
   $preview.innerHTML = Prism.highlight(
     $code.value,
     Prism.languages.javascript,
     "javascript",
   );
 }
+// insert two spaces on tab
+$code.addEventListener("keydown", (ev) => {
+  if (ev.code === "Tab") {
+    ev.preventDefault();
+    $code.setRangeText("  ", $code.selectionStart, $code.selectionStart, "end");
+    mirror();
+  }
+});
 
 $code.addEventListener("input", mirror);
